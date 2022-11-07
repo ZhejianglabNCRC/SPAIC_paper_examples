5c8a2da8b1fbfa7c7ab6b440d4054d54b5bbdf2a
hongchaofei <hongchf@zhejianglab.com>
2022-11-07 02:52:45 +0000

    Case study 1.

--- CorticalColumnTest.py
+++ CorticalColumnTest.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on 2021/8/5
-@project: main.py
+@project: SPAIC
 @filename: CorticalColumnTest
 @author: Hong Chaofei
 @contact: hongchf@gmail.com
@@ -12,11 +12,8 @@ Created on 2021/8/5
 import spaic
 import torch
 
-from tqdm import tqdm
-from spaic.IO.Dataset import MNIST as dataset
 import numpy as np
 import matplotlib.pyplot as plt
-from multiprocessing import Queue, Process, Lock
 from matplotlib.animation import FuncAnimation
 from time import time
 
@@ -24,68 +21,67 @@ from time import time
 # 设备设置
 if torch.cuda.is_available():
     device = 'cuda'
-
+    print('cuda')
 else:
     device = 'cpu'
-print(device)
-e_w = 0.02
-i_w = -0.08
+e_w = 0.03
+i_w = -0.06
 
 class Layer(spaic.Assembly):
 
     def __init__(self, exc_num, inh_num, ee_p, ii_p, ei_p, ie_p):
         super(Layer, self).__init__()
-        self.excitory_neuron = spaic.NeuronGroup(neuron_number=exc_num, neuron_type='exc', neuron_model='lif', tau_m=20.0)
-        self.inhibitory_neuron = spaic.NeuronGroup(neuron_number=inh_num, neuron_type='inh', neuron_model='lif', tau_m=10.0)
+        self.excitory_neuron = spaic.NeuronGroup(num=exc_num, neuron_type='exc', model='clif', tau_m=20.0, tau_p=12.0, tau_q=8.0)
+        self.inhibitory_neuron = spaic.NeuronGroup(num=inh_num, neuron_type='inh', model='clif', tau_m=30.0, tau_p=20.0, tau_q=12.0)
 
         self.exc_possion = spaic.Generator(num=exc_num, dec_target=self.excitory_neuron, coding_method='poisson_generator',
-                                             coding_var_name='Isyn', rate=0.01)
+                                             coding_var_name='Isyn', rate=0.02, weight=0.4)
         self.inh_possion = spaic.Generator(num=inh_num, dec_target=self.inhibitory_neuron, coding_method='poisson_generator',
-                                             coding_var_name='Isyn', rate=0.0)
-        self.ee_connection = spaic.Connection(pre_assembly=self.excitory_neuron, post_assembly=self.excitory_neuron,
-                                                link_type='sparse_connection',  density=ee_p, w_min=0, w_mean=e_w)
-        self.ei_connection = spaic.Connection(pre_assembly=self.excitory_neuron, post_assembly=self.inhibitory_neuron,
-                                                link_type='sparse_connection',  density=ei_p, w_min=0, w_mean=e_w)
-        self.ie_connection = spaic.Connection(pre_assembly=self.inhibitory_neuron, post_assembly=self.excitory_neuron,
-                                                link_type='sparse_connection',  density=ie_p, w_max=0, w_mean=i_w)
-        self.ii_connection = spaic.Connection(pre_assembly=self.inhibitory_neuron, post_assembly=self.inhibitory_neuron,
-                                                link_type='sparse_connection',  density=ii_p, w_max=0, w_mean=i_w)
+                                             coding_var_name='Isyn', rate=0.02, weight=0.4)
+        self.ee_connection = spaic.Connection(pre=self.excitory_neuron, post=self.excitory_neuron,
+                                              link_type='sparse', density=ee_p, w_min=0, w_mean=e_w)
+        self.ei_connection = spaic.Connection(pre=self.excitory_neuron, post=self.inhibitory_neuron,
+                                              link_type='sparse', density=ei_p, w_min=0, w_mean=e_w)
+        self.ie_connection = spaic.Connection(pre=self.inhibitory_neuron, post=self.excitory_neuron,
+                                              link_type='sparse', density=ie_p, w_max=0, w_mean=i_w)
+        self.ii_connection = spaic.Connection(pre=self.inhibitory_neuron, post=self.inhibitory_neuron,
+                                              link_type='sparse', density=ii_p, w_max=0, w_mean=i_w)
 
 class LayerProj(spaic.Projection):
 
     def __init__(self, pre : Layer, post: Layer, ee_p, ei_p, ie_p, ii_p):
         super(LayerProj, self).__init__(pre, post)
         if ee_p > 0:
-            self.ee_connection = spaic.Connection(pre_assembly=pre.excitory_neuron, post_assembly=post.excitory_neuron,
-                                                  link_type='sparse_connection', density=ee_p, w_min=0, w_mean=e_w)
+            self.ee_connection = spaic.Connection(pre=pre.excitory_neuron, post=post.excitory_neuron,
+                                                  link_type='sparse', density=ee_p, w_min=0, w_mean=e_w)
         if ei_p > 0:
-            self.ei_connection = spaic.Connection(pre_assembly=pre.excitory_neuron, post_assembly=post.inhibitory_neuron,
-                                                  link_type='sparse_connection', density=ei_p, w_min=0, w_mean=e_w)
+            self.ei_connection = spaic.Connection(pre=pre.excitory_neuron, post=post.inhibitory_neuron,
+                                                  link_type='sparse', density=ei_p, w_min=0, w_mean=e_w)
         if ie_p > 0:
-            self.ie_connection = spaic.Connection(pre_assembly=pre.excitory_neuron, post_assembly=post.inhibitory_neuron,
-                                                  link_type='sparse_connection', density=ie_p, w_max=0, w_mean=i_w)
+            self.ie_connection = spaic.Connection(pre=pre.inhibitory_neuron, post=post.excitory_neuron,
+                                                  link_type='sparse', density=ie_p, w_max=0, w_mean=i_w)
         if ii_p > 0:
-            self.ii_connection = spaic.Connection(pre_assembly=pre.excitory_neuron, post_assembly=post.inhibitory_neuron,
-                                                  link_type='sparse_connection', density=ii_p, w_max=0, w_mean=i_w)
+            self.ii_connection = spaic.Connection(pre=pre.inhibitory_neuron, post=post.inhibitory_neuron,
+                                                  link_type='sparse', density=ii_p, w_max=0, w_mean=i_w)
 
 class CorticalMicrocircuit(spaic.Network):
 
     def __init__(self):
         super(CorticalMicrocircuit, self).__init__()
-        self.layer23 = Layer(exc_num=2068, inh_num=583, ee_p=0.101, ei_p=0.125, ii_p=0.289, ie_p=0.227)
-        self.layer4 = Layer(exc_num=2191, inh_num=548, ee_p=0.05, ei_p=0.079, ii_p=0.26, ie_p=0.175)
-        self.layer5 = Layer(exc_num=485, inh_num=106, ee_p=0.083, ei_p=0.06, ii_p=0.416, ie_p=0.323)
-        self.layer6 = Layer(exc_num=1439, inh_num=395, ee_p=0.07, ei_p=0.066, ii_p=0.544, ie_p=0.385)
+        self.layer23 = Layer(exc_num=2068, inh_num=583, ee_p=0.101, ei_p=0.125, ii_p=0.189, ie_p=0.327)
+        self.layer4 = Layer(exc_num=2191, inh_num=548, ee_p=0.05, ei_p=0.079, ii_p=0.26, ie_p=0.135)
+        self.layer5 = Layer(exc_num=485, inh_num=106, ee_p=0.083, ei_p=0.06, ii_p=0.316, ie_p=0.423)
+        self.layer6 = Layer(exc_num=1439, inh_num=395, ee_p=0.04, ei_p=0.066, ii_p=0.144, ie_p=0.455)
 
         self.proj23_4 = LayerProj(pre=self.layer23, post=self.layer4, ee_p=0.008, ei_p=0.069, ie_p=0.0, ii_p=0)
-        self.proj23_5 = LayerProj(pre=self.layer23, post=self.layer5, ee_p=0.01, ei_p=0.0095, ie_p=0.062, ii_p=0.027)
-        self.proj4_23 = LayerProj(pre=self.layer4, post=self.layer23, ee_p=0.024, ei_p=0.032, ie_p=0.01, ii_p=0)
-        self.proj4_5 = LayerProj(pre=self.layer4, post=self.layer5, ee_p=0.051, ei_p=0.0026, ie_p=0.02, ii_p=0)
-        self.proj4_6 = LayerProj(pre=self.layer4, post=self.layer6, ee_p=0.031, ei_p=0.0013, ie_p=0.01, ii_p=0)
+        self.proj23_5 = LayerProj(pre=self.layer23, post=self.layer5, ee_p=0.01, ei_p=0.095, ie_p=0.062, ii_p=0.027)
+        self.proj4_23 = LayerProj(pre=self.layer4, post=self.layer23, ee_p=0.024, ei_p=0.042, ie_p=0.02, ii_p=0)
+        self.proj4_5 = LayerProj(pre=self.layer4, post=self.layer5, ee_p=0.041, ei_p=0.026, ie_p=0.02, ii_p=0)
+        self.proj4_6 = LayerProj(pre=self.layer4, post=self.layer6, ee_p=0.021, ei_p=0.023, ie_p=0.01, ii_p=0)
         self.proj5_6 = LayerProj(pre=self.layer5, post=self.layer6, ee_p=0.0037, ei_p=0.002, ie_p=0.007, ii_p=0.0)
         self.proj6_5 = LayerProj(pre=self.layer6, post=self.layer5, ee_p=0.002, ei_p=0.001, ie_p=0.001, ii_p=0)
 
-        self.layer23_v = spaic.StateMonitor(self.layer23.excitory_neuron, var_name='Isyn')
+        self.layer23_v = spaic.StateMonitor(self.layer23.excitory_neuron, var_name='V')
 
         self.l23exc_spk_monitor = spaic.SpikeMonitor(self.layer23.excitory_neuron)
         self.l23inh_spk_monitor = spaic.SpikeMonitor(self.layer23.inhibitory_neuron)
@@ -98,17 +94,14 @@ class CorticalMicrocircuit(spaic.Network):
 
 
 TestNet = CorticalMicrocircuit()
+TestNet.set_backend('pytorch','cuda')
+TestNet.build()
 
-
-
-
-TestNet.set_backend('pytorch',device)
 with torch.no_grad():
     t1 = time()
-    TestNet.run(100.0)
+    TestNet.run(1000.0)
     t2 = time()
     print(t2-t1)
-
 
 
 # tim = TestNet.layer23_v.times
